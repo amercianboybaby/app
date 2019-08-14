645768000fa165b7ac6cfbb6f5f90642b7c55ea8
yangjun <1415995502@qq.com>
2019-08-14 15:13:34 +0800

    maven分模块项目

---
+++ util/src/main/java/org/com/pcitc/app/utils/RegexUtils.java
@@ -0,0 +1,15 @@
+package org.com.pcitc.app.utils;
+
+/**
+ * 名称：<pre>正则工具类</pre> <br></br>
+ * 作者：Alon <br></br>
+ * 创建时间：2019-08-14 <br></br>
+ * 功能描述：<br></br>
+ */
+public class RegexUtils {
+
+    private final String BLANK = ""; /* 空白正则 */
+    private final String EMAIL = ""; /* 邮件正则 */
+    private final String TELEPHONE = ""; /* 手机正则 */
+
+}
---
+++ util/src/main/java/org/com/pcitc/app/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package org.com.pcitc.app.utils;
+
+/**
+ * 名称：<pre>字符串工具类</pre> <br></br>
+ * 作者：Alon <br></br>
+ * 创建时间：2019-08-14 <br></br>
+ * 功能描述：<br></br>
+ */
+public class StringUtils extends org.apache.commons.lang.StringUtils {
+
+
+
+}
